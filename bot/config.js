fe12b36e1bea15377960a2baa6519881e5528568
Janis <76851529+DerTyp876@users.noreply.github.com>
2023-05-11 14:23:53 +0200

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -1,7 +1,7 @@
 const config = {
-  clientId: "1037396501732794489",
+  clientId: "",
   token:
-    "MTAzNzM5NjUwMTczMjc5NDQ4OQ.G-vEdY.yH-xVPVzErIPSLwCJ0AdNfcH-vPHX_dvNHLm3o",
+    "",
 };
 
 module.exports = config;
